32995e7137ecf9baf6f36f2db512f5124ac64a71
paoloevan <paoloev1900@gmail.com>
2022-11-08 13:41:06 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+ * 
+ * Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+ */
+
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+           
+        }
+    }
+}).mount('#app')
