591e6fa49561e7e8a98a60bff7cf513895f1f088
paoloevan <paoloev1900@gmail.com>
2022-11-08 14:07:17 +0100

    add emails list

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,16 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-           
+           myEmail: []
+        }
+    },
+    mounted(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.myEmail.push(response.data.response)
+            })
         }
     }
 }).mount('#app')
